efa21911b2efb1dd6c35d1d03c329477d2a6bbdd
Alejandro Torrejon <alejandro.cartografia@gmail.com>
2024-11-15 16:09:45 -0300

    Initial commit

---
+++ src/main/java/br/com/atflow/spring_boot_api_receita/SpringBootApiReceitaApplication.java
@@ -0,0 +1,13 @@
+package br.com.atflow.spring_boot_api_receita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiReceitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiReceitaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atflow/spring_boot_api_receita/SpringBootApiReceitaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atflow.spring_boot_api_receita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiReceitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
